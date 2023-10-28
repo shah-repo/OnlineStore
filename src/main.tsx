f12053aff5a93e80a0ff7bc033536a87e3dc7690
shah-cloud-III <sfahed@enlil.com>
2023-10-28 09:22:12 +0530

    added url base name

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/OnlineStore'>
       <App />
     </Router>
   </React.StrictMode>,
